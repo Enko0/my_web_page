c9b853e4c22895df8d73c18dedbea55519fc1dd9
byambatsogt enkhbat <byambaa1982@gmail.com>
2024-12-04 18:13:01 -0600

    added new file

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash, Response
+import hashlib
+import pandas as pd
+import sqlite3
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+import os
+import requests
+
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Needed for session management
+
+
+
+
+@app.route("/pokemon", methods=["GET", "POST"])
+def index():
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
